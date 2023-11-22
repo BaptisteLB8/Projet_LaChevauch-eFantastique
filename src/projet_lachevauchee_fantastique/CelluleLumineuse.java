07ed808f09550e03a23fb836ed8ae3b64793e23b
bapti <bapti@LAPTOP-OARLVC8V.education.lakanal.epf.fr>
2023-11-22 11:02:05 +0100

    Avancée cellule lumineuse

--- src/projet_lachevauchee_fantastique/CelluleLumineuse.java
+++ src/projet_lachevauchee_fantastique/CelluleLumineuse.java
@@ -9,5 +9,79 @@ package projet_lachevauchee_fantastique;
  * @author Utilisateur
  */
 public class CelluleLumineuse {
-    
-}
+
+    private boolean etat;
+
+    /**
+     * initialise l'etat en éteint
+     */
+    public CelluleLumineuse() {
+        etat = false;
+    }
+
+    /**
+     * initialise en allumer
+     */
+    public void allumer() {
+        etat = true;
+    }
+
+    /**
+     * initialise en éteint
+     *
+     * @return: renvoie éteint
+     */
+    public void eteindre() {
+        etat = false;
+    }
+
+    /**
+     * active la cellule lumineuse en inversant son etat
+     *
+     * @return: renvoie l'inverse de son etat
+     */
+    public void activerCellule() {
+        etat = !etat;
+    }
+
+    /**
+     * éteint la cellule
+     *
+     * @return: éteint
+     */
+    public void eteindreCellule() {
+        etat = false;
+
+    }
+
+    /**
+     * vérifie si la cellule est actuellement éteint
+     *
+     * @return 
+     * @return: renvoie l'inverse de son état
+     */
+    public boolean estEteint() {
+        return !etat;
+    }
+
+    /**
+     * donne l'état actuel de la cellule
+     *
+     * @return état
+     */
+    public boolean getEtat() {
+        return etat;
+    }
+
+    /**
+     * donne comme valeur X si elle est allumé 
+     *
+     * @return X si elle est allumé 
+     */
+    @Override
+    public String toString() {
+        if (etat) {
+            return "X";
+    }
+
+
