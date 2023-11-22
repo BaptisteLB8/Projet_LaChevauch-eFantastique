b8c20ec19dfa3ebb8364e72e953b87046ed28155
Utilisateur <Utilisateur@DESKTOP-96N4K6B.education.lakanal.epf.fr>
2023-11-22 09:59:36 +0100

    Avancée grille de jeu

--- src/projet_lachevauchee_fantastique/GrilleDeJeu.java
+++ src/projet_lachevauchee_fantastique/GrilleDeJeu.java
@@ -1,5 +1,7 @@
 package projet_lachevauchee_fantastique;
 
+import java.util.Random;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -11,4 +13,34 @@ package projet_lachevauchee_fantastique;
  */
 public class GrilleDeJeu {
     
+    CelluleLumineuse[][] matriceCellules;
+    int nbLignes;
+    int nbColonnes;
+    static Random generateur_aleat = new Random();
+
+    /**
+     *Permet de représente une grille de cellules lumineuses
+     * @param p_nbLignes
+     * @param p_nbColonnes
+     */
+    public GrilleDeJeu(int p_nbLignes, int p_nbColonnes) {
+        nbLignes = p_nbLignes;
+        nbColonnes = p_nbColonnes;
+        matriceCellules = new CelluleLumineuse[nbLignes][nbColonnes];
+
+        for (int i = 0; i < nbLignes; i++) {
+            for (int j = 0; j < nbColonnes; j++) {
+                matriceCellules[i][j] = new CelluleLumineuse();
+            }
+        }
+    }
+    
+    public void eteindreToutesLesCellules() {
+        for (int i = 0; i < nbLignes; i++) {
+            for (int j = 0; j < nbColonnes; j++) {
+                matriceCellules[i][j].eteindreCellule();
+            }
+        }
+    }
+
 }
